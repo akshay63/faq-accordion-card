d41959155d5a704f711d679441e7ea8a20405cf3
Akshaye <meshakshay9860u@gmail.com>
2023-02-17 19:45:53 +0530

    Desktop View completed

---
+++ script.js
@@ -0,0 +1,23 @@
+const ul = document.querySelector(".accordian");
+
+function accordian(evt) {
+  evt.preventDefault();
+  let btn = evt.target.closest("button");
+  if (!btn) return;
+  if (!ul.contains(btn)) return;
+
+  let panel = btn.nextElementSibling;
+  let panelArrIcon = btn.querySelector("img");
+  btn.classList.toggle("open");
+  panel.classList.toggle("open");
+
+  if (btn.classList.contains("open") && panel.classList.contains("open")) {
+    panelArrIcon.style.transform = "rotate(180deg)";
+  } else {
+    panelArrIcon.style.transform = "rotate(0deg)";
+  }
+
+  console.log();
+}
+
+ul.addEventListener("click", accordian, false);
